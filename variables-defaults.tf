c23913c9407fc5ba1f1cbce335f2f890e2445b8f
Gary Taylor <gary.taylor@hismessages.com>
2022-01-17 07:53:57 +0000

    Added initial clone of tax tribunals

---
+++ variables-defaults.tf
@@ -0,0 +1,33 @@
+# Variables with default values or values specified in an {env}.tfvars file
+
+variable "location" {
+  default = "UK South"
+}
+
+variable "common_tags" {
+  type = map(string)
+}
+
+# Database
+
+variable "db_storage_mb" {
+  default = "5120"
+}
+
+variable "component" {
+  default = "service"
+}
+
+variable "db_version" {
+  default = 11
+}
+
+variable "db_postgresql_user" {
+  default = "employment_tribunals_postgresql_user"
+}
+
+# Keyvault
+
+variable "product_group_name" {
+  default = "dcd_group_pet_v2"
+}
