7f49de5493325a134bd6ed6892f024a8112bca9f
mokainos <72137340+mokainos@users.noreply.github.com>
2022-01-27 12:14:49 +0000

    Update et1privateendpoint.tf

--- et1privateendpoint.tf
+++ et1privateendpoint.tf
@@ -1,18 +1,18 @@
 resource "azurerm_resource_group" "et1petrg" {
   provider = "azurerm.pet-aks"
-  name     = "${var.product}-et1-${var.env}-endpoint-rg"
+  name     = "${var.product}-et1-${local.petenv}-endpoint-rg"
   location = var.location
 
 }
 resource "azurerm_private_endpoint" "et1petpostgres" {
   provider            = "azurerm.pet-aks"
-  name                = "${var.product}-et1-${var.env}-endpoint"
+  name                = "${var.product}-et1-${local.petenv}-endpoint"
   location            = var.location
   resource_group_name = azurerm_resource_group.et1petrg.name
   subnet_id           = data.azurerm_subnet.petpostgres.id
 
   private_service_connection {
-    name                           = "${var.product}-et1-${var.env}-postgressdb"
+    name                           = "${var.product}-et1-${local.petenv}-postgressdb"
     private_connection_resource_id = module.et1-database.id
     subresource_names              = ["postgresqlServer"]
     is_manual_connection           = false
