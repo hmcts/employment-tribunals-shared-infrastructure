a291dfb83f9a77cb00e21fec3f2902a8c377999c
mokainos <72137340+mokainos@users.noreply.github.com>
2022-01-27 12:15:39 +0000

    Update et3privateendpoint.tf

--- et3privateendpoint.tf
+++ et3privateendpoint.tf
@@ -1,18 +1,18 @@
 resource "azurerm_resource_group" "et3petrg" {
   provider = "azurerm.pet-aks"
-  name     = "${var.product}-et3-${var.env}-endpoint-rg"
+  name     = "${var.product}-et3-${local.petenv}-endpoint-rg"
   location = var.location
 
 }
 resource "azurerm_private_endpoint" "et3petpostgres" {
   provider            = "azurerm.pet-aks"
-  name                = "${var.product}-et3-${var.env}-endpoint"
+  name                = "${var.product}-et3-${local.petenv}-endpoint"
   location            = var.location
   resource_group_name = azurerm_resource_group.et3petrg.name
   subnet_id           = data.azurerm_subnet.petpostgres.id
 
   private_service_connection {
-    name                           = "${var.product}-et3-${var.env}-postgressdb"
+    name                           = "${var.product}-et3-${local.petenv}-postgressdb"
     private_connection_resource_id = module.et3-database.id
     subresource_names              = ["postgresqlServer"]
     is_manual_connection           = false
