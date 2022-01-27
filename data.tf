1c1974d8689c92712f0c1f48ce20bbbca0cf9ec1
mokainos <72137340+mokainos@users.noreply.github.com>
2022-01-27 13:56:59 +0000

    update

--- data.tf
+++ data.tf
@@ -1,13 +1,16 @@
-data "azurerm_subnet" "petpostgres" {
-  provider             = "azurerm.pet-aks"
-  name                 = "pet_private_${local.petenv}"
-  virtual_network_name = "pet_${local.petenv}_network"
-  resource_group_name  = "pet_${local.petenv}_network_resource_group"
-}
+//data "azurerm_subnet" "petpostgres" {
+//  provider             = "azurerm.pet-aks"
+//  name                 = "pet_private_${local.petenv}"
+//  virtual_network_name = "pet_${local.petenv}_network"
+//  resource_group_name  = "pet_${local.petenv}_network_resource_group"
+//}
 
 locals {
   petenv = var.env == "prod" || var.env == "stg" ? var.env : "dev"
 }
 output "petnetwork" {
   value = "pet_${local.petenv}_network"
+}
+output "varenv" {
+    value = var.env
 }
