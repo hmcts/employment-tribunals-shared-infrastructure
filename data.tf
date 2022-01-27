9ef4951d11b1f95afab384ad0d4cf43f0e8b0eb6
mokainos <72137340+mokainos@users.noreply.github.com>
2022-01-27 11:28:39 +0000

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,1 +1,6 @@
-
+data "azurerm_subnet" "petpostgres" {
+  provider             = "azurerm.pet-aks"
+  name                 = "pet_private_${var.env}"
+  virtual_network_name = "pet_${var.env}_network"
+  resource_group_name  = "pet_${var.env}_network_resource_group"
+}
